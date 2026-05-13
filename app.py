14a940b07bacce132422dd13981aad198d2f4784
ningnong-0139 <littlenear291@gmail.com>
2026-05-13 14:15:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ st.set_page_config(page_title="MyApp", layout="wide")
 
 st.title("🏠 หน้าหลัก ")
 st.write("### Boot Camp: Data Science and Machine Learning")
+st.write("xxxningnong0139xxx")
 st.info("7 Day Intensive Hands-on Workshop")
 st.write("##### Day 1: การจัดการข้อมูลพื้นฐานและโครงสร้างข้อมูลด้วย Python")
 
