b50f7607be768ef4b2cdf89e186525c72fe019d1
ningnong-0139 <littlenear291@gmail.com>
2026-06-04 16:18:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,6 @@ elif st.button("พยากรณ์ยอดการขาย"):
     st.switch_page("pages/sale_predict.py")
 elif st.button("การพยากรณ์ระยะเวลาการให้บริการขนส่ง"):
     st.switch_page("pages/truck_predict.py")
+elif st.button("การจำแนกประเภทข้อมูลยอดขาย"):
+    st.switch_page("pages/classify_redbull_sale.py")
 
