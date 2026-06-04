f845a93dfa9ce129d18ed6fc4aa6a436036af78f
ningnong-0139 <littlenear291@gmail.com>
2026-06-04 11:36:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,6 @@ elif st.button("การแปลงข้อมูล"):
     st.switch_page("page/transform_app.py")
 elif st.button("การวิเคราะห์ข้อมูลเชิงสำรวจ"):
     st.switch_page("page/EDA_app.py")
-elif st.botton("พยากรณ์ยอดการขาย"):
+elif st.button("พยากรณ์ยอดการขาย"):
     st.switch_page(page/sale_predict.py)
 
