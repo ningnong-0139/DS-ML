8ba14fbdcc02414050186c036df72e542fc16e37
ningnong-0139 <littlenear291@gmail.com>
2026-06-10 13:27:07 +0700

    Add button for clustering segment page navigation

--- app.py
+++ app.py
@@ -23,4 +23,6 @@ elif st.button("การพยากรณ์ระยะเวลาการ�
     st.switch_page("pages/truck_predict.py")
 elif st.button("การจำแนกประเภทข้อมูลยอดขาย"):
     st.switch_page("pages/classify_redbull_sale.py")
+elif st.button("การจัดกลุ่ม"):
+    st.switch_page("pages/clustering_segment.py")
 
